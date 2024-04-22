1f041d2e6ced279512236f6563ba04a0209c809b
Renohun <127425333+Renohun@users.noreply.github.com>
2024-04-22 18:18:08 +0200

    added the create the "create" buttons + funcionality

--- wpf_password_app/MainWindow.xaml.cs
+++ wpf_password_app/MainWindow.xaml.cs
@@ -34,7 +34,16 @@ namespace wpf_password_app
                 {
                     if (pCreate_Box.Password.Contains("0") || pCreate_Box.Password.Contains("1") || pCreate_Box.Password.Contains("2") || pCreate_Box.Password.Contains("3") || pCreate_Box.Password.Contains("4") || pCreate_Box.Password.Contains("5") || pCreate_Box.Password.Contains("6") || pCreate_Box.Password.Contains("7") || pCreate_Box.Password.Contains("8") || pCreate_Box.Password.Contains("9"))
                     {
-                        FileStream fs = new FileStream("master_pw.txt", FileMode.Create); StreamWriter sw = new StreamWriter(fs, Encoding.UTF8);
+
+                        string dir = @"C:\Temp\wpf_pw_app";
+
+                        if (!Directory.Exists(dir)) { Directory.CreateDirectory(dir); }
+
+                        string subdir = @"C:\Temp\wpf_pw_app\pws";
+
+                        if (!Directory.Exists(subdir)) { Directory.CreateDirectory(subdir); }
+
+                        FileStream fs = new FileStream(@"C:\\Temp\\wpf_pw_app\m_pw.txt", FileMode.Create); StreamWriter sw = new StreamWriter(fs, Encoding.UTF8);
 
                         //shit password enrypction
 
@@ -95,7 +104,7 @@ namespace wpf_password_app
 
         private void Actual_Log_In_Button(object sender, RoutedEventArgs e)
         {
-            FileStream fs_log_in = new FileStream("master_pw.txt", FileMode.Open); StreamReader sr = new StreamReader(fs_log_in, Encoding.UTF8);
+            FileStream fs_log_in = new FileStream(@"C:\\Temp\\wpf_pw_app\\m_pw.txt", FileMode.Open); StreamReader sr = new StreamReader(fs_log_in, Encoding.UTF8);
 
             // shit password decryption
 
@@ -115,19 +124,18 @@ namespace wpf_password_app
                 MessageBox.Show("Successfull Log In!", "Log In", MessageBoxButton.OK, MessageBoxImage.Information);
                     
 
-                //Ide fog jonni a fo program resz, DE NINCSEN RENDES DESIGN avagy normalis elrendezes
+                //NINCSEN RENDES DESIGN avagy normalis elrendezes
 
                 pCreate_Box.Visibility = Visibility.Hidden; Log_In_Text.Visibility = Visibility.Hidden; Real_Log_In.Visibility = Visibility.Hidden;
 
+                delete_1.Visibility = Visibility.Visible; delete_2.Visibility = Visibility.Visible; delete_3.Visibility = Visibility.Visible; delete_4.Visibility = Visibility.Visible;
 
+                create_1.Visibility = Visibility.Visible; create_2.Visibility = Visibility.Visible; create_3.Visibility = Visibility.Visible; create_4.Visibility = Visibility.Visible;
+                 
+                box_1.Visibility = Visibility.Visible; box_2.Visibility = Visibility.Visible; box_3.Visibility = Visibility.Visible; box_4.Visibility = Visibility.Visible;
 
-
-
-
-
-
-
-            }   
+                show_1.Visibility = Visibility.Visible; show_2.Visibility = Visibility.Visible; show_3.Visibility = Visibility.Visible; show_4.Visibility = Visibility.Visible;
+            }
             else
             {
                 pCreate_Box.Clear();
@@ -138,12 +146,251 @@ namespace wpf_password_app
         {
             try
             {
-                File.Delete("C:\\Users\\Kornel\\Desktop\\wpf_password_app\\wpf_password_app\\bin\\Debug\\net8.0-windows\\master_pw.txt");
+                File.Delete(@"C:\\Temp\\wpf_pw_app\\m_pw.txt");
             }
-            catch(Exception) //erdekes, egy olyan kene ahol az ez alatt levo messageboxot megjeleniti ha a file nem letezik, de nem akarja, FileNotExceptionnal sem
+            catch(Exception) //erdekes, egy olyan kene ahol az ez alatt levo messageboxot megjeleniti ha a file nem letezik, de nem akarja, FileNotExceptionnal sem, mert jo lenne majd a kesobbi dolgokhoz
             {        
                 MessageBox.Show("The File dosen't exists", "Delete Error", MessageBoxButton.OK, MessageBoxImage.Error);
             }
+        }
+
+
+        //delete buttons
+
+        private void delete_pw_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void delete_pw_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void delete_pw_3(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void delete_pw_4(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        //create buttons
+
+        private void create_bt_1(object sender, RoutedEventArgs e)
+        {
+            if (box_1.Password.Length < 5)
+            {
+                box_1.Clear();
+
+                MessageBox.Show("The password was too short. It must be at least 6 charachters long", "Register Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else
+            {
+                if (box_1.Password.Contains("#") || box_1.Password.Contains("$") || box_1.Password.Contains("&") || box_1.Password.Contains("%"))
+                {
+                    if (box_1.Password.Contains("0") || box_1.Password.Contains("1") || box_1.Password.Contains("2") || box_1.Password.Contains("3") || box_1.Password.Contains("4") || box_1.Password.Contains("5") || box_1.Password.Contains("6") || box_1.Password.Contains("7") || box_1.Password.Contains("8") || box_1.Password.Contains("9"))
+                    {
+                        FileStream fs = new FileStream(@"C:\\Temp\\wpf_pw_app\\pws\\pw_1.txt", FileMode.Create); StreamWriter sw = new StreamWriter(fs, Encoding.UTF8);
+
+                        //shit password enrypction
+
+                        List<int> list_ASCII = new List<int>();
+
+                        for (int i = 0; i < box_1.Password.Length; i++)
+                        {
+                            list_ASCII.Add(Convert.ToInt32(box_1.Password[i]));
+                        }
+
+                        for (int i = 0; i < list_ASCII.Count; i++)
+                        {
+                            sw.Write(list_ASCII[i] * 6421);
+                        }
+
+                        sw.Close(); fs.Close();
+
+                        MessageBox.Show("Password created successfully! Please Log in!", "Password Created", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                        box_1.Clear();
+                    }
+                    else
+                    {
+                        box_1.Clear(); MessageBox.Show("The password has to contain numbers", "Register Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                }
+                else
+                {
+                    box_1.Clear(); MessageBox.Show("The password has to contain at least one of these special characters #, $, %, &;", "Register Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+        private void create_bt_2(object sender, RoutedEventArgs e)
+        {
+            if (box_2.Password.Length < 5)
+            {
+                box_2.Clear();
+
+                MessageBox.Show("The password was too short. It must be at least 6 charachters long", "Register Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else
+            {
+                if (box_2.Password.Contains("#") || box_2.Password.Contains("$") || box_2.Password.Contains("&") || box_2.Password.Contains("%"))
+                {
+                    if (box_2.Password.Contains("0") || box_2.Password.Contains("1") || box_2.Password.Contains("2") || box_2.Password.Contains("3") || box_2.Password.Contains("4") || box_2.Password.Contains("5") || box_2.Password.Contains("6") || box_2.Password.Contains("7") || box_2.Password.Contains("8") || box_2.Password.Contains("9"))
+                    {
+                        FileStream fs = new FileStream(@"C:\\Temp\\wpf_pw_app\\pws\\pw_2.txt", FileMode.Create); StreamWriter sw = new StreamWriter(fs, Encoding.UTF8);
+
+                        //shit password enrypction
+
+                        List<int> list_ASCII = new List<int>();
+
+                        for (int i = 0; i < box_2.Password.Length; i++)
+                        {
+                            list_ASCII.Add(Convert.ToInt32(box_2.Password[i]));
+                        }
+
+                        for (int i = 0; i < list_ASCII.Count; i++)
+                        {
+                            sw.Write(list_ASCII[i] * 6421);
+                        }
+
+                        sw.Close(); fs.Close();
+
+                        MessageBox.Show("Password created successfully! Please Log in!", "Password Created", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                        box_2.Clear();
+                    }
+                    else
+                    {
+                        box_2.Clear(); MessageBox.Show("The password has to contain numbers", "Register Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                }
+                else
+                {
+                    box_2.Clear(); MessageBox.Show("The password has to contain at least one of these special characters #, $, %, &;", "Register Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+        private void create_bt_3(object sender, RoutedEventArgs e)
+        {
+            if (box_3.Password.Length < 5)
+            {
+                box_3.Clear();
+
+                MessageBox.Show("The password was too short. It must be at least 6 charachters long", "Register Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else
+            {
+                if (box_3.Password.Contains("#") || box_3.Password.Contains("$") || box_3.Password.Contains("&") || box_3.Password.Contains("%"))
+                {
+                    if (box_3.Password.Contains("0") || box_3.Password.Contains("1") || box_3.Password.Contains("2") || box_3.Password.Contains("3") || box_3.Password.Contains("4") || box_3.Password.Contains("5") || box_3.Password.Contains("6") || box_3.Password.Contains("7") || box_3.Password.Contains("8") || box_3.Password.Contains("9"))
+                    {
+                        FileStream fs = new FileStream(@"C:\\Temp\\wpf_pw_app\\pws\\pw_2.txt", FileMode.Create); StreamWriter sw = new StreamWriter(fs, Encoding.UTF8);
+
+                        //shit password enrypction
+
+                        List<int> list_ASCII = new List<int>();
+
+                        for (int i = 0; i < box_3.Password.Length; i++)
+                        {
+                            list_ASCII.Add(Convert.ToInt32(box_3.Password[i]));
+                        }
+
+                        for (int i = 0; i < list_ASCII.Count; i++)
+                        {
+                            sw.Write(list_ASCII[i] * 6421);
+                        }
+
+                        sw.Close(); fs.Close();
+
+                        MessageBox.Show("Password created successfully! Please Log in!", "Password Created", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                        box_3.Clear();
+                    }
+                    else
+                    {
+                        box_3.Clear(); MessageBox.Show("The password has to contain numbers", "Register Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                }
+                else
+                {
+                    box_3.Clear(); MessageBox.Show("The password has to contain at least one of these special characters #, $, %, &;", "Register Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+        private void create_bt_4(object sender, RoutedEventArgs e)
+        {
+            {
+                if (box_4.Password.Length < 5)
+                {
+                    box_4.Clear();
+
+                    MessageBox.Show("The password was too short. It must be at least 6 charachters long", "Register Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                else
+                {
+                    if (box_4.Password.Contains("#") || box_4.Password.Contains("$") || box_4.Password.Contains("&") || box_4.Password.Contains("%"))
+                    {
+                        if (box_4.Password.Contains("0") || box_4.Password.Contains("1") || box_4.Password.Contains("2") || box_4.Password.Contains("3") || box_4.Password.Contains("4") || box_4.Password.Contains("5") || box_4.Password.Contains("6") || box_4.Password.Contains("7") || box_4.Password.Contains("8") || box_4.Password.Contains("9"))
+                        {
+                            FileStream fs = new FileStream(@"C:\\Temp\\wpf_pw_app\\pws\\pw_2.txt", FileMode.Create); StreamWriter sw = new StreamWriter(fs, Encoding.UTF8);
+
+                            //shit password enrypction
+
+                            List<int> list_ASCII = new List<int>();
+
+                            for (int i = 0; i < box_4.Password.Length; i++)
+                            {
+                                list_ASCII.Add(Convert.ToInt32(box_4.Password[i]));
+                            }
+
+                            for (int i = 0; i < list_ASCII.Count; i++)
+                            {
+                                sw.Write(list_ASCII[i] * 6421);
+                            }
+
+                            sw.Close(); fs.Close();
+
+                            MessageBox.Show("Password created successfully! Please Log in!", "Password Created", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                            box_4.Clear();
+                        }
+                        else
+                        {
+                            box_4.Clear(); MessageBox.Show("The password has to contain numbers", "Register Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                        }
+                    }
+                    else
+                    {
+                        box_4.Clear(); MessageBox.Show("The password has to contain at least one of these special characters #, $, %, &;", "Register Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                }
+            }
+        }
+
+        //show buttons
+
+        private void show_bt_1(object sender, RoutedEventArgs e)
+        {
+            FileStream fs_show_1 = new FileStream(@"C:\\Temp\\wpf_pw_app\\pws\\pw_1.txt", FileMode.Open); StreamReader sr_show_1 = new StreamReader(fs_show_1, Encoding.Default);
+
+            string pw_show = "";
+
+            while(!sr_show_1.EndOfStream)
+            {
+                pw_show = sr_show_1.ReadLine().TrimEnd();
+            }
+        }
+        private void show_bt_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void show_bt_3(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void show_bt_4(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
